04d9e98abdec5229cfc0c0006e3cc4e625551d95
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-04-30 22:20:26 +0530

    Added solution - LeetHub

--- Sum of Array Elements - GFG/sum-of-array-elements.cpp
+++ Sum of Array Elements - GFG/sum-of-array-elements.cpp
@@ -15,8 +15,8 @@ int sumElement(int arr[],int n)
 {
     //Your code here
     int sum = 0;
-    for(int i = 0; i < n; i++){
-        sum += arr[i];
+    for(int j = 0; j < n; j++){
+        sum += arr[j];
     }
     return sum;
 }
