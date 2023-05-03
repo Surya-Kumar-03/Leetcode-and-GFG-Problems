45172c475dcc02aeb44a1cfc11ebe1c76a339385
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-03 13:11:49 +0530

    Time: 49 ms (86.08%) | Memory: 31.8 MB (62.90%) - LeetSync

--- find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        //using stl
-        vector<int> v1, v2;
-        set<int> s1(nums1.begin(), nums1.end()), s2(nums2.begin(), nums2.end());
-        // back_inserter used to store the result
-        set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(v1));
-        set_difference(s2.begin(), s2.end(), s1.begin(), s1.end(), back_inserter(v2));
-        return {v1, v2};
-        
         // My approach
-    //    unordered_map<int, bool> ourMap;
-    //    int n1 = nums1.size();
-    //    int n2 = nums2.size();
-    //    for(int i = 0; i < n1; i++){
-    //         ourMap[nums1[i]] = false;
-    //    }
+       unordered_map<int, bool> ourMap;
+       int n1 = nums1.size();
+       int n2 = nums2.size();
+       for(int i = 0; i < n1; i++){
+            ourMap[nums1[i]] = false;
+       }
 
-    //    vector<int> ans2;
-    //    for(int i = 0; i < n2; i++){
-    //        if(ourMap.find(nums2[i]) == ourMap.end()){
-    //            ans2.push_back(nums2[i]);
-    //        } 
-    //        ourMap[nums2[i]] = true;
-    //    }
+       vector<int> ans2;
+       for(int i = 0; i < n2; i++){
+           if(ourMap.find(nums2[i]) == ourMap.end()){
+               ans2.push_back(nums2[i]);
+           } 
+           ourMap[nums2[i]] = true;
+       }
 
-    //    vector<int> ans1;
-    //    for(auto ele: ourMap){
-    //        if(ele.second == false){
-    //            ans1.push_back(ele.first);
-    //        }
-    //    }
-    //    return {ans1, ans2};
+       vector<int> ans1;
+       for(auto ele: ourMap){
+           if(ele.second == false){
+               ans1.push_back(ele.first);
+           }
+       }
+       return {ans1, ans2};
+    
+        //using stl
+        // vector<int> v1, v2;
+        // set<int> s1(nums1.begin(), nums1.end()), s2(nums2.begin(), nums2.end());
+        // // back_inserter used to store the result
+        // set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(v1));
+        // set_difference(s2.begin(), s2.end(), s1.begin(), s1.end(), back_inserter(v2));
+        // return {v1, v2};
+    
     }
 };
