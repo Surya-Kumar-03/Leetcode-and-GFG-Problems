568aaf85e82894c8971c9b4554dc42fac935a119
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-03 16:21:38 +0530

    Time: 40 ms (51.07%) | Memory: 71.4 MB (84.49%) - LeetSync

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -11,34 +11,16 @@
 class Solution {
 public:
     ListNode* ourAdder(ListNode* l1, ListNode* l2, int& carry){
-        if(!l1 && !l2 && carry != 0) {
-            return new ListNode(carry);
-        } else if(!l1 && !l2) {
+        if(!l1 && !l2 && carry == 0) {
             return NULL;
-        } else if(!l1 && carry == 0) {
-            return l2;
-        } else if(!l2 && carry == 0) {
-            return l1;
-        } else {
-            int sum = 0;
-            if(l1){
-                sum += l1->val;
-            }
-            if(l2){
-                sum += l2->val;
-            }
-            sum += carry;
-            carry = sum / 10; //carry will be 1 if greater than 9
-            ListNode* newNode = new ListNode(sum % 10);
-            if(l1 && l2) {
-                newNode->next = ourAdder(l1-> next, l2-> next, carry);
-            } else if(l1){
-                newNode->next = ourAdder(l1-> next, NULL, carry);
-            } else {
-                newNode->next = ourAdder(NULL, l2-> next, carry);
-            }
-            return newNode;
         }
+        int sum = carry;
+        if(l1) sum += l1->val;
+        if(l2) sum += l2->val;
+        carry = sum / 10; //carry will be 1 if greater than 9
+        ListNode* newNode = new ListNode(sum % 10);
+        newNode->next = ourAdder(l1 ? l1->next : NULL, l2 ? l2->next : NULL, carry);
+        return newNode;
     }
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
