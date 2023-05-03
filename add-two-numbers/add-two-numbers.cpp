42335abcd576b70c8159343d8ab4b5ba414ef9e2
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-03 16:39:19 +0530

    Time: 43 ms (39.88%) | Memory: 71.4 MB (84.49%) - LeetSync

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -11,21 +11,44 @@
 class Solution {
 public:
     // O(max(N, M)) O(max(N, M))
-    ListNode* ourAdder(ListNode* l1, ListNode* l2, int& carry){
-        if(!l1 && !l2 && carry == 0) {
-            return NULL;
-        }
-        int sum = carry;
-        if(l1) sum += l1->val;
-        if(l2) sum += l2->val;
-        carry = sum / 10; //carry will be 1 if greater than 9
-        ListNode* newNode = new ListNode(sum % 10);
-        newNode->next = ourAdder(l1 ? l1->next : NULL, l2 ? l2->next : NULL, carry);
-        return newNode;
-    }
+    // ListNode* ourAdder(ListNode* l1, ListNode* l2, int& carry){
+    //     if(!l1 && !l2 && carry == 0) {
+    //         return NULL;
+    //     }
+    //     int sum = carry;
+    //     if(l1) sum += l1->val;
+    //     if(l2) sum += l2->val;
+    //     carry = sum / 10; //carry will be 1 if greater than 9
+    //     ListNode* newNode = new ListNode(sum % 10);
+    //     newNode->next = ourAdder(l1 ? l1->next : NULL, l2 ? l2->next : NULL, carry);
+    //     return newNode;
+    // }
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0;
-        return ourAdder(l1, l2, carry);
+        // int carry = 0;
+        // return ourAdder(l1, l2, carry); RECURSIVE
+        // ITERATIVE
+        int sum = l1->val + l2->val;
+        int carry = sum/10; 
+        ListNode* head = new ListNode(sum%10);
+        ListNode* node = head;
+        l1 = l1->next;
+        l2 = l2->next;
+        while(l1 || l2 || carry){
+            sum = carry;
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            } 
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            } 
+            carry = sum/10;
+            ListNode* newNode = new ListNode(sum % 10);
+            node->next = newNode;
+            node = node->next;
+        }   
+        return head;
     }
 };
