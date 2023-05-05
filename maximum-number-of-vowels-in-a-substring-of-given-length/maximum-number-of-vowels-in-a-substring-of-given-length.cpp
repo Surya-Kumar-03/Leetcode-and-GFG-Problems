0d079d0869cc011ee5ff3b474da73f399a9f400e
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-05 18:58:38 +0530

    Time: 26 ms (40.98%) | Memory: 9.9 MB (95.70%) - LeetSync

--- maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,31 +1,42 @@
-class Solution {
+class Solution
+{
 public:
-    bool isVowel(char c) {
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+    bool isVowel(char c)
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        {
             return true;
-        } else return false;
+        }
+        else
+            return false;
     }
-    int maxVowels(string s, int k) {
+    int maxVowels(string s, int k)
+    {
         int curVowel = 0;
         int j;
-        for(j = 0; j < k; j++) {
-            if(isVowel(s[j])) {
+        for (j = 0; j < k; j++)
+        {
+            if (isVowel(s[j]))
+            {
                 curVowel++;
             }
         }
         int maxVowel = curVowel;
         int i = 0;
         j = k;
-        while(j < s.size()){
-            if(isVowel(s[i])){
+        while (j < s.size())
+        {
+            if (isVowel(s[i]))
+            {
                 curVowel--;
-            } 
+            }
 
-                if(isVowel(s[j])){
-                    curVowel++;
-                    maxVowel = max(maxVowel, curVowel);
-                }
-            
+            if (isVowel(s[j]))
+            {
+                curVowel++;
+                maxVowel = max(maxVowel, curVowel);
+            }
+
             i++, j++;
         }
         return maxVowel;
