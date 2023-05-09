274451f6ddc727e890002c271b17c272f0012e7a
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-09 13:42:30 +0530

    Time: 12 ms (32.22%) | Memory: 14.7 MB (81.73%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,38 +11,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1 || !list2) {
-            if(!list1) return list2;
-            else return list1;
-        }
-        ListNode* head = NULL;
-        ListNode* ans = NULL;
-        while(list1 != NULL && list2 != NULL) {
-            if(list1->val < list2->val) {
-                if(head == NULL){
-                    head = list1;
-                    ans = head;
-                } 
-                else {
-                    head -> next = list1;
-                    head = head->next;
-                }
+        if (!list1) return list2;
+        if (!list2) return list1;
+        
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
                 list1 = list1->next;
-            } else  {
-                if(head == NULL) {
-                    head = list2;
-                    ans = head;
-                }
-                else {
-                    head -> next = list2;
-                    head = head->next;
-                }
+            } else {
+                tail->next = list2;
                 list2 = list2->next;
             }
+            tail = tail->next;
         }
-        if(list1 != NULL) {
-            head -> next = list1;
-        } else head -> next = list2;
+        
+        if (list1) tail->next = list1;
+        else tail->next = list2;
+        
+        ListNode* ans = dummy->next;
+        delete dummy;
         return ans;
     }
 };
