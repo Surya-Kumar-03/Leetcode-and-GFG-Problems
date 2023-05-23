85d73c1dcd26e0e7b1eeba6b45a48c4f77944498
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-05-23 15:24:50 +0530

    Time: 12 ms (41.95%) | Memory: 18.8 MB (50.03%) - LeetSync

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,32 +12,32 @@
 class Solution
 {
     public:
-        int maxDepth(TreeNode *root)
-        {
-            if (root == NULL) return 0;
-            queue<TreeNode*> pending;
-            pending.push(root);
-            int level = 0;
-            while (!pending.empty())
-            {
-                int size = pending.size();
-                for (int i = 0; i < size; i++)
-                {
-                    TreeNode *node = pending.front();
-                    pending.pop();
-                    if (node->left) pending.push(node->left);
-                    if (node->right) pending.push(node->right);
-                }
-                level++;
-            }
-            return level;
-        }
+        // int maxDepth(TreeNode *root)
+        // {
+        //     if (root == NULL) return 0;
+        //     queue<TreeNode*> pending;
+        //     pending.push(root);
+        //     int level = 0;
+        //     while (!pending.empty())
+        //     {
+        //         int size = pending.size();
+        //         for (int i = 0; i < size; i++)
+        //         {
+        //             TreeNode *node = pending.front();
+        //             pending.pop();
+        //             if (node->left) pending.push(node->left);
+        //             if (node->right) pending.push(node->right);
+        //         }
+        //         level++;
+        //     }
+        //     return level;
+        // }
 
-   	// int maxDepth(TreeNode* root) {
-   	//    	// recursive
-   	//     if(root == NULL) return 0;
-   	//     int lh = maxDepth(root->left);
-   	//     int rh = maxDepth(root->right);
-   	//     return 1 + max(lh, rh);
-   	// }
+   	int maxDepth(TreeNode* root) {
+   	   	// recursive
+   	    if(root == NULL) return 0;
+   	    int lh = maxDepth(root->left);
+   	    int rh = maxDepth(root->right);
+   	    return 1 + max(lh, rh);
+   	}
 };
