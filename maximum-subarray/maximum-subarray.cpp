929cdb392a47d88ee5013e8b5e8d88aa0c0d1bd2
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-01 15:46:42 +0530

    Time: 118 ms (67.39%) | Memory: 67.6 MB (88.71%) - LeetSync

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,3 +1,4 @@
+// Practise
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -10,3 +11,24 @@ public:
         return maxSum;
     }
 };
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         //optimised approach is to use kadane's algorithm
+//         int curSum = 0;
+//         int maxSum = INT_MIN;
+     
+//         for(int i = 0;i<nums.size();i++)
+//         {   
+//             // curSum += nums[i]; only will work for positive
+//             // below will work for negative as well
+//             curSum = max(curSum + nums[i] , nums[i]);
+//             if(curSum > maxSum)
+//                 maxSum = curSum;
+//         }
+//         return maxSum;
+//         //brute force is to calculate all subarrays sum
+//     }
+// };
+
