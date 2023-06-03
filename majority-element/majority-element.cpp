2f5091cfdc7cb8418955a9df91e1df8bbac60f38
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-03 13:48:58 +0530

    Time: 10 ms (98.58%) | Memory: 19.6 MB (64.34%) - LeetSync

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+        int majorityElement(vector<int> &nums)
+        {
+            int major = nums[0], count = 1;
+            for (int i = 1; i < nums.size(); i++)
+            {
+                if (nums[i] == major)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count == 0)
+                    {
+                        major = nums[i];
+                        count = 1;
+                    }
+                }
+            }
+            return major;
+        }
+};
