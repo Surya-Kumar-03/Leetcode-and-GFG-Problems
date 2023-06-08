ef6505dd2e45a4bf72a80946db25d2628c5ccf9c
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-08 13:24:50 +0530

    Time: 15 ms (82.04%) | Memory: 10.4 MB (71.33%) - LeetSync

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -3,17 +3,41 @@ public:
     int countNegatives(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        int searchRow = 0;
         int negCount = 0;
-        for(int i = cols - 1; i >= 0; i--) {
-            for(int j = searchRow; j < rows; j++) {
-                if(grid[j][i] < 0) {
-                    negCount += (rows - j);
-                    searchRow = j;
-                    break;
-                }
+        
+        int r = 0;
+        int c = cols - 1;
+        
+        while (r < rows && c >= 0) {
+            if (grid[r][c] < 0) {
+                negCount += rows - r;
+                c--;
+            } else {
+                r++;
             }
         }
+        
         return negCount;
     }
 };
+
+
+// class Solution {
+// public:
+//     int countNegatives(vector<vector<int>>& grid) {
+//         int rows = grid.size();
+//         int cols = grid[0].size();
+//         int searchRow = 0;
+//         int negCount = 0;
+//         for(int i = cols - 1; i >= 0; i--) {
+//             for(int j = searchRow; j < rows; j++) {
+//                 if(grid[j][i] < 0) {
+//                     negCount += (rows - j);
+//                     searchRow = j;
+//                     break;
+//                 }
+//             }
+//         }
+//         return negCount;
+//     }
+// };
