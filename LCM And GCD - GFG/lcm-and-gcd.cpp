17b07172fd08b2b43826695ee6890d5af182d184
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-12 00:14:08 +0530

    Added solution - LeetHub

--- LCM And GCD - GFG/lcm-and-gcd.cpp
+++ LCM And GCD - GFG/lcm-and-gcd.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 // } Driver Code Ends
-
-#include<boost/math/common_factor.hpp>
 class Solution {
   public:
     vector<long long> lcmAndGcd(long long A , long long B) {
         // code here
-        vector<long long> ans;
-        ans.push_back(boost::math::lcm(A, B));
-        ans.push_back(__gcd(A, B));
-        return ans;
+        long long a = A, b = B;
+        while (b != 0) {
+            long long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        long long gcd = a;
+        
+        // Calculate LCM
+        long long lcm = (A * B) / gcd;
+        
+        return {lcm, gcd};
     }
+    
+    // vector<long long> lcmAndGcd(long long A , long long B) {
+    //     // code here
+    //     vector<long long> ans;
+    //     ans.push_back((A*B)/__gcd(A,B));
+    //     ans.push_back(__gcd(A, B));
+    //     return ans;
+    // }
 };
 
 //{ Driver Code Starts.
