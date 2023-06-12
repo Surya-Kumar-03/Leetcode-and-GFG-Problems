ad3f03dbd39a42f1ad149b16b0cfc027cbb1cf58
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-12 09:01:54 +0530

    Added solution - LeetHub

---
+++ Sum of first n terms - GFG/sum-of-first-n-terms.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long sumOfSeries(long long N) {
+        // code here
+        if(N == 0) return 0;
+        return ((N*(N + 1)) / 2) * ((N * (N+1))/2);
+        // return (long long)pow(N, 3) + sumOfSeries(N - 1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
