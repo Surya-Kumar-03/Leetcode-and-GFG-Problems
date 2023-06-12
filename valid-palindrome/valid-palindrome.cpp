b585ff1fb30ad17700b0c3dece63b7c3985bde2e
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-12 19:58:22 +0530

    Time: 3 ms (91.65%) | Memory: 7.4 MB (74.80%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            while (i < j && !(isalpha(s[i]) || isdigit(s[i]))) {
+                i++;
+            }
+            while (i < j && !(isalpha(s[j]) || isdigit(s[j]))) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+            i++, j--;
+        }
+        return true;
+    }
+};
