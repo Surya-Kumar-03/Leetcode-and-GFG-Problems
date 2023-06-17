4e5cd4ccc02844a516c2107c52abc48bbe03f0f4
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-17 12:49:11 +0530

    Added solution - LeetHub

---
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        int i = 0;
+        while(true) {
+            if(arr[i] == 0) break;
+            i++;
+        }
+        
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] == 1){
+                swap(arr[j], arr[i]);
+                i++;
+            } 
+        }
+        reverse(arr, arr+n);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.segregate0and1(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
