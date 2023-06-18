50e2d478fa2240c3e5a1849591a3084289cabf37
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-19 00:03:26 +0530

    Time: 256 ms (70.80%) | Memory: 98.9 MB (99.19%) - LeetSync

--- frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -11,6 +11,7 @@ public:
         for (long long end = 0; end < n; ++end) {
             sum += nums[end]; 
             // See neetcode video for why we did this
+            // means that if we muliply the window size * the number we are trying to make the max freq, it should be less than the total sum + k(extra budget)
             while ((end - start + 1) * nums[end] > sum + k) {
                 sum -= nums[start]; 
                 start++; 
