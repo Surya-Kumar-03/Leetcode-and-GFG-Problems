18537c5963f33b5d34c44ed99b914c97b68537ba
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-19 21:25:12 +0530

    Time: 7 ms (60.63%) | Memory: 8.5 MB (14.31%) - LeetSync

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -10,62 +10,13 @@
  */
 class Solution {
 public:
-    ListNode*  reverseList(ListNode* head) {
-        ListNode* newHead = NULL;
-        while(head != NULL) {
-            ListNode* temp = head -> next;
-            head -> next = newHead;
-            newHead = head;
-            head = temp;
-        }
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head -> next == NULL) return head;
+        ListNode* newHead = reverseList(head->next);
+        ListNode* temp = head;
+        temp = temp -> next;
+        temp -> next = head;
+        head -> next = NULL;
         return newHead;
     }
-
-    // Recursive Practise
-    // ListNode* reverseList(ListNode* head){
-    //     if(!head || !head->next) return head;
-    //     ListNode* newHead = reverseList(head->next);
-    //     head->next->next = head; //the last node in the reversed guy to point to our node
-    //     head->next = NULL;  // our node next  is null now
-    //     return newHead;
-    // }
-
-
-    // Recursive
-    // ListNode* reverseList(ListNode* head) {
-    // if(head == NULL || head->next == NULL)
-    //     return head;
-    //     ListNode* newHead = reverseList(head->next);
-    //     head->next->next = head;
-    //     head->next = NULL;
-    //     return newHead;
-    // }
-    
-    // Iterative
-    // ListNode* reverseList(ListNode* head) {
-    //     ListNode* newHead = NULL;
-    //     while(head != NULL)
-    //     {
-    //         ListNode* next = head->next;
-    //         head->next = newHead;
-    //         newHead = head;
-    //         head = next;
-    //     }
-    //     return newHead;
-    // }
-
-    // my practise
-    // ListNode* reverseList(ListNode* head) {
-    //     if(head == NULL || head->next == NULL) return head;
-    //     ListNode* prevNext = head -> next;
-    //     ListNode* lastNode = head;
-    //     lastNode -> next = NULL;
-    //     while(prevNext != NULL) {
-    //         ListNode* temp = prevNext -> next;
-    //         prevNext -> next = lastNode;
-    //         lastNode = prevNext;
-    //         prevNext = temp;
-    //     }
-    //     return lastNode;
-    // }
 };
