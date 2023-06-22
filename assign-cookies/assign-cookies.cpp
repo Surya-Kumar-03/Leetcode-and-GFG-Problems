6a0b53495aded429d7276334d5345a138a4d8150
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-06-22 12:53:23 +0530

    Time: 22 ms (98.17%) | Memory: 17.4 MB (55.43%) - LeetSync

--- assign-cookies/assign-cookies.cpp
+++ assign-cookies/assign-cookies.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int contentCount = 0, j = 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        for(int i = 0; i < g.size(); i++) {
-            while(j < s.size() && g[i] > s[j]) {
-                j++;
-            } 
-            if(j >= s.size()) break;
-            else {
-                if(g[i] <= s[j]) {
-                    contentCount++;
-                    j++;
-                } else break;
-            }
+        sort(g.begin(), g.end()); //sorting helps us get the minimum size cookie
+        sort(s.begin(), s.end());  //             for a guy
+        int i = 0; // iterating in people array
+        // j iterates through cookies array
+        // run loop till there are cookies and people left
+        for(int j = 0; i < g.size() && j < s.size(); j++) {
+            if(g[i] <= s[j]) i++; // this guy is satisfied
         }
-        return contentCount;
+        return i; //it holds the satisfied guys
     }
+
+    // My Brute force
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     int contentCount = 0, j = 0;
+    //     sort(g.begin(), g.end());
+    //     sort(s.begin(), s.end());
+    //     for(int i = 0; i < g.size(); i++) {
+    //         while(j < s.size() && g[i] > s[j]) {
+    //             j++;
+    //         } 
+    //         if(j >= s.size()) break;
+    //         else {
+    //             if(g[i] <= s[j]) {
+    //                 contentCount++;
+    //                 j++;
+    //             } else break;
+    //         }
+    //     }
+    //     return contentCount;
+    // }
 };
