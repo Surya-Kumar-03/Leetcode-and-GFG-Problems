b3cefa68f881e581c7cbff74c97ad23098150d80
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-01 21:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (95.07%) - LeetSync

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1) return n;
-        vector<int> dp(n + 1);
-        dp[0] = 0;
-        dp[1] = 1;
+        int lastprev = 1;
+        int prev = 0;
+        int cur = 0;
+        if(n == 1) return 1;
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            cur = prev + lastprev;
+            prev = lastprev;
+            lastprev = cur;
         }
-        return dp[n];
+        return cur;
     }
 };
