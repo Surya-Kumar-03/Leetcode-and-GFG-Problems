0ee9b3f76eb02ebd865f1c5aa778cb90690fae0e
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-02 18:55:45 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -9,18 +9,31 @@ using namespace std;
 class Solution{
 public:	
 	// calculate the maximum sum with out adjacent
-	int maxSum(int *arr, int n, int i, vector<int>& memo) {
-	    if(i >= n) return 0;
-	    if(memo[i] != -1) return memo[i];
-	    int pick = arr[i] + maxSum(arr, n, i + 2, memo);
-	    int notPick = maxSum(arr, n, i + 1, memo);
-	    return memo[i] = max(pick, notPick);
-	}
+// 	int maxSum(int *arr, int n, int i, vector<int>& memo) {
+// 	    if(i >= n) return 0;
+// 	    if(memo[i] != -1) return memo[i];
+// 	    int pick = arr[i] + maxSum(arr, n, i + 2, memo);
+// 	    int notPick = maxSum(arr, n, i + 1, memo);
+// 	    return memo[i] = max(pick, notPick);
+// 	}
 	
 	int findMaxSum(int *arr, int n) {
-	    // code here
-	    vector<int> memo(n, -1);
-	    return maxSum(arr, n, 0, memo);
+	   // code here
+	   // vector<int> memo(n, -1);
+	   // return maxSum(arr, n, 0, memo);
+	   
+	   vector<int> dp(n, 0);
+	   for(int j = n - 1; j >= 0; j--) {
+	       int pick = arr[j], notPick = INT_MIN;
+	       if(j + 2 < n) {
+	           pick += dp[j + 2];
+	       }
+	       if(j + 1 < n) {
+	           notPick = dp[j + 1];
+	       }
+	       dp[j] = max(pick, notPick);
+	   }
+	   return dp[0];
 	}
 };
 
