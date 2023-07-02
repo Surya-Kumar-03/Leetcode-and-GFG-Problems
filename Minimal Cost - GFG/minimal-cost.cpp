932bc601bde3730cf9e412dcdffb0226986c5bca
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-02 14:24:40 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,22 +6,34 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int cost(vector<int>& height, int n, int k, int i, vector<int>& memo) {
-        if(i == n - 1) return 0;
-        if(memo[i] != -1) return memo[i];
-        int minCost = INT_MAX;
-        for(int j = 0; j < k; j++) {
-            if(j + i + 1 >= n) break;
-            int curContender = cost(height, n, k, i + j + 1, memo) + 
-                abs(height[j + i + 1] - height[i]);
-            minCost = min(minCost, curContender);
-        }
-        return memo[i] = minCost;
-    }
+    // int cost(vector<int>& height, int n, int k, int i, vector<int>& memo) {
+    //     if(i == n - 1) return 0;
+    //     if(memo[i] != -1) return memo[i];
+    //     int minCost = INT_MAX;
+    //     for(int j = 0; j < k; j++) {
+    //         if(j + i + 1 >= n) break;
+    //         int curContender = cost(height, n, k, i + j + 1, memo) + 
+    //             abs(height[j + i + 1] - height[i]);
+    //         minCost = min(minCost, curContender);
+    //     }
+    //     return memo[i] = minCost;
+    // }
     
     int minimizeCost(vector<int>& height, int n, int k) {
-        vector<int> memo(n + 1, -1);
-        return cost(height, n, k, 0, memo);
+        // vector<int> memo(n + 1, -1);
+        // return cost(height, n, k, 0, memo);
+        
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < k; j++) {
+                if(j + i + 1 >= n) break;
+                int curContender = dp[j + i + 1] +
+                    abs(height[j + i + 1] - height[i]);
+                dp[i] = min(dp[i], curContender);
+            }
+        }
+        return dp[0];
     }
 };
 
