0d044c563084d8cb9515a3d38a76a8bff6dd3fc5
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-02 12:01:48 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -6,21 +6,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int ourReach(vector<int>& height, int n, int i, vector<int>& memo) {
-        if(i == n - 1) return 0;
-        if(memo[i] != -1) return memo[i];
-        int left = INT_MAX, right = INT_MAX;
-        if(i + 1 < n)
-            left = ourReach(height, n, i + 1, memo) + abs(height[i + 1] - height[i]);
-        if(i + 2 < n)
-            right = ourReach(height, n, i + 2, memo) + abs(height[i + 2] - height[i]);
-        return memo[i] = min(left, right);
-    }
+    // int ourReach(vector<int>& height, int n, int i, vector<int>& memo) {
+    //     if(i == n - 1) return 0;
+    //     if(memo[i] != -1) return memo[i];
+    //     int left = INT_MAX, right = INT_MAX;
+    //     if(i + 1 < n)
+    //         left = ourReach(height, n, i + 1, memo) + abs(height[i + 1] - height[i]);
+    //     if(i + 2 < n)
+    //         right = ourReach(height, n, i + 2, memo) + abs(height[i + 2] - height[i]);
+    //     return memo[i] = min(left, right);
+    // }
     
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
-        vector<int> memo(n, -1);
-        return ourReach(height, n, 0, memo);
+        // vector<int> memo(n, -1);
+        // return ourReach(height, n, 0, memo);
+        vector<int> dp(n);
+        dp[n - 1] = 0;
+        for(int j = n - 2; j >= 0; j--) {
+            int oneJump = INT_MAX;
+            int twoJump = INT_MAX;
+            if(j + 1 < n) oneJump = dp[j + 1] + abs(height[j + 1] - height[j]);
+            if(j + 2 < n) twoJump = dp[j + 2] + abs(height[j + 2] - height[j]);
+            dp[j] = min(oneJump, twoJump);
+        }
+        return dp[0];
     }
 };
 
