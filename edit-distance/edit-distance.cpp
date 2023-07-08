95beb937d1101ae17e02d925a4d1d96c50f08a32
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-08 18:33:11 +0530

    Time: 20 ms (41.23%) | Memory: 8.9 MB (61.42%) - LeetSync

--- edit-distance/edit-distance.cpp
+++ edit-distance/edit-distance.cpp
@@ -20,7 +20,21 @@ public:
     int minDistance(string word1, string word2) {
         int rows = word1.size();
         int cols = word2.size();
-        vector<vector<int>> memo(rows, vector<int>(cols, -1));
-        return ourMin(word1, word2, 0, 0, memo);
+        // vector<vector<int>> memo(rows, vector<int>(cols, -1));
+        // return ourMin(word1, word2, 0, 0, memo);
+        vector<vector<int>> dp(rows + 1, vector<int>(cols + 1,  0)); 
+        for(int i = 0; i <= rows; i++) dp[i][0] = i;
+        for(int j = 0; j <= cols; j++) dp[0][j] = j;
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= cols; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i][j - 1], 
+                        min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        } 
+        return dp[rows][cols];
     }
 };
