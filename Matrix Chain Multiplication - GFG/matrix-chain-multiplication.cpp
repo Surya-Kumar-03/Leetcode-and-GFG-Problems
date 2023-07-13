8b6e3705b8b3df49aeb7c3084e608e01e7ff7889
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-13 19:15:31 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,30 +9,47 @@ using namespace std;
 
 class Solution{
 public:
-    int ourMatrix(int arr[],int i, int j, vector<vector<int>>& memo) {
-        if(i == j) {
-            return 0; // No Operations needed
-        }   
+    // int ourMatrix(int arr[],int i, int j, vector<vector<int>>& memo) {
+    //     if(i == j) {
+    //         return 0; // No Operations needed
+    //     }   
         
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
+    //     if(memo[i][j] != -1) {
+    //         return memo[i][j];
+    //     }
         
-        int minSteps = INT_MAX;
-        for(int k = i; k < j; k++) {
-            int curPartition = ourMatrix(arr, i, k, memo) + 
-                    ourMatrix(arr, k + 1, j, memo) +
-                    (arr[i - 1] * arr[k] * arr[j]);
-            minSteps = min(minSteps, curPartition);
-        }
-        return memo[i][j] = minSteps;
-    }
+    //     int minSteps = INT_MAX;
+    //     for(int k = i; k < j; k++) {
+    //         int curPartition = ourMatrix(arr, i, k, memo) + 
+    //                 ourMatrix(arr, k + 1, j, memo) +
+    //                 (arr[i - 1] * arr[k] * arr[j]);
+    //         minSteps = min(minSteps, curPartition);
+    //     }
+    //     return memo[i][j] = minSteps;
+    // }
     
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>> memo(N, vector<int>(N, -1));
-        return ourMatrix(arr, 1, N - 1, memo);
+        // vector<vector<int>> memo(N, vector<int>(N, -1));
+        // return ourMatrix(arr, 1, N - 1, memo);
+        
+        vector<vector<int>> dp(N, vector<int>(N, 0));
+    
+        for (int len = 2; len < N; len++) {
+            for (int i = 1; i < N - len + 1; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+                
+                for (int k = i; k < j; k++) {
+                    int curPartition = dp[i][k] + dp[k + 1][j] + 
+                                (arr[i - 1] * arr[k] * arr[j]);
+                    dp[i][j] = min(dp[i][j], curPartition);
+                }
+            }
+        }
+        
+        return dp[1][N - 1];
     }
 };
 
