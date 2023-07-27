fe0986ec16203d5ddda4416ec53d143215d18940
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-07-27 21:30:09 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,35 +7,64 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order.
-	void dfs(int source, vector<bool>& visited, vector<int> adj[], 
-	    stack<int>& nodes) {
+// 	void dfs(int source, vector<bool>& visited, vector<int> adj[], 
+// 	    stack<int>& nodes) {
 	    
-	    visited[source] = true;
-	    for(auto adjacentNode : adj[source]) {
-	        if(!visited[adjacentNode]) {
-	            dfs(adjacentNode, visited, adj, nodes);
-	        }
-	    }
-	    nodes.push(source);
-	}
+// 	    visited[source] = true;
+// 	    for(auto adjacentNode : adj[source]) {
+// 	        if(!visited[adjacentNode]) {
+// 	            dfs(adjacentNode, visited, adj, nodes);
+// 	        }
+// 	    }
+// 	    nodes.push(source);
+// 	}
 	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    // code here
-	    vector<bool> visited(V, false);
-	    stack<int> nodes;
-	    for(int i = 0; i < V; i++) {
-	        if(!visited[i]) {
-	            dfs(i, visited, adj, nodes);
-	        }
-	    }
+	   // code here
+	   // vector<bool> visited(V, false);
+	   // stack<int> nodes;
+	   // for(int i = 0; i < V; i++) {
+	   //     if(!visited[i]) {
+	   //         dfs(i, visited, adj, nodes);
+	   //     }
+	   // }
 	    
-	    vector<int> ans;
-	    while(!nodes.empty()) {
-	        ans.push_back(nodes.top());
-	        nodes.pop();
-	    }
-	    return ans;
+	   // vector<int> ans;
+	   // while(!nodes.empty()) {
+	   //     ans.push_back(nodes.top());
+	   //     nodes.pop();
+	   // }
+	   // return ans;
+	   
+	   //Kahn's Algorithm BFS
+	   vector<int> indegree(V, 0);
+	   for(int i = 0; i < V; i++) {
+	       for(int j = 0; j < adj[i].size(); j++) {
+	           indegree[adj[i][j]]++; 
+	       }
+	   }
+	   
+	   queue<int> pending;
+	   for(int i = 0; i < V; i++) {
+	       if(indegree[i] == 0) {
+	           pending.push(i);
+	       }
+	   }
+	   
+	   vector<int> ans;
+	   while(!pending.empty()) {
+	       int topEle = pending.front();
+	       ans.push_back(topEle);
+	       pending.pop();
+	       for(auto neighbour : adj[topEle]) {
+	           indegree[neighbour]--;
+	           if(indegree[neighbour] == 0) {
+	               pending.push(neighbour);
+	           }
+	       }
+	   }
+	   return ans;
 	}
 };
 
