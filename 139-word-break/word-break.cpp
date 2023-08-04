edaed22bd2d78617b9ce98ef3654d214eaa5cf7f
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-04 14:20:35 +0530

    Time: 33 ms (16.17%) | Memory: 21.7 MB (8.52%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    bool ourWord(string s, int i, int j, unordered_set<string>& dict,
-            vector<vector<int>>& memo) {
-        int n = s.size();
-        if(i >= n) return true; //string is valid
-        if(j >= n) return false; //invalid
+    // bool ourWord(string s, int i, int j, unordered_set<string>& dict,
+    //         vector<vector<int>>& memo) {
+    //     int n = s.size();
+    //     if(i >= n) return true; //string is valid
+    //     if(j >= n) return false; //invalid
 
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
+    //     if(memo[i][j] != -1) {
+    //         return memo[i][j];
+    //     }
 
-        string cur = s.substr(i, j - i + 1);
-        bool breakHere = false, noBreak;
-        if(dict.find(cur) != dict.end()) {
-            breakHere = ourWord(s, j + 1, j + 1, dict, memo);
-        }
-        noBreak = ourWord(s, i, j + 1, dict, memo);
-        return memo[i][j] = breakHere || noBreak;
-    }
+    //     string cur = s.substr(i, j - i + 1);
+    //     bool breakHere = false, noBreak;
+    //     if(dict.find(cur) != dict.end()) {
+    //         breakHere = ourWord(s, j + 1, j + 1, dict, memo);
+    //     }
+    //     noBreak = ourWord(s, i, j + 1, dict, memo);
+    //     return memo[i][j] = breakHere || noBreak;
+    // }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         int n = s.size();
-        vector<vector<int>> memo(n, vector<int>(n, -1));
-        return ourWord(s, 0, 0, dict, memo);
+        // vector<vector<int>> memo(n, vector<int>(n, -1));
+        // return ourWord(s, 0, 0, dict, memo);
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+        for(int i = n; i >= 0; i--) {
+            for(int j = n; j >= 0; j--) {
+                string cur = s.substr(i, j - i + 1);
+                bool breakHere = false, noBreak;
+                if(dict.find(cur) != dict.end()) {
+                    if(j + 1 >= n) {
+                        breakHere = true;
+                    }
+                    else breakHere = dp[j + 1][j + 1];
+                }
+                if(j + 1 >= n) {
+                    noBreak = false;
+                }
+                else noBreak = dp[i][j + 1];
+                dp[i][j] = breakHere || noBreak;
+            }
+        }
+        return dp[0][0];
     }
 };
