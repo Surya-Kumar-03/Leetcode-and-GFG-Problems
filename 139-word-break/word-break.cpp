a52439b0543cb68cda951fe527ddf891a23a87c8
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-04 14:32:59 +0530

    Time: 11 ms (57.90%) | Memory: 13.2 MB (38.95%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -19,30 +19,50 @@ public:
     //     return memo[i][j] = breakHere || noBreak;
     // }
 
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     unordered_set<string> dict(wordDict.begin(), wordDict.end());
+    //     int n = s.size();
+    //     // vector<vector<int>> memo(n, vector<int>(n, -1));
+    //     // return ourWord(s, 0, 0, dict, memo);
+
+    //     vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+    //     for(int i = n; i >= 0; i--) {
+    //         for(int j = n; j >= 0; j--) {
+    //             string cur = s.substr(i, j - i + 1);
+    //             bool breakHere = false, noBreak;
+    //             if(dict.find(cur) != dict.end()) {
+    //                 if(j + 1 >= n) {
+    //                     breakHere = true;
+    //                 }
+    //                 else breakHere = dp[j + 1][j + 1];
+    //             }
+    //             if(j + 1 >= n) {
+    //                 noBreak = false;
+    //             }
+    //             else noBreak = dp[i][j + 1];
+    //             dp[i][j] = breakHere || noBreak;
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         int n = s.size();
-        // vector<vector<int>> memo(n, vector<int>(n, -1));
-        // return ourWord(s, 0, 0, dict, memo);
 
-        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
-        for(int i = n; i >= 0; i--) {
-            for(int j = n; j >= 0; j--) {
+        vector<bool> dp(n + 1, false);
+        dp[n] = true; // Empty string is always breakable
+
+        for(int i = n - 1; i >= 0 ; i--) {
+            for(int j = i; j < n; j++) {
                 string cur = s.substr(i, j - i + 1);
-                bool breakHere = false, noBreak;
-                if(dict.find(cur) != dict.end()) {
-                    if(j + 1 >= n) {
-                        breakHere = true;
-                    }
-                    else breakHere = dp[j + 1][j + 1];
+                // cur word is there in dict and the rest of the words are also there
+                if(dict.find(cur) != dict.end() && dp[j + 1]) {
+                    dp[i] = true;
+                    break; // No need to check further, substring s[i:n) is breakable
                 }
-                if(j + 1 >= n) {
-                    noBreak = false;
-                }
-                else noBreak = dp[i][j + 1];
-                dp[i][j] = breakHere || noBreak;
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
