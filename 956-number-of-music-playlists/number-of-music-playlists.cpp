503b731b587895097be560e8ebcd362c032a09b4
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-06 10:31:39 +0530

    Time: 3 ms (83.78%) | Memory: 9.1 MB (33.33%) - LeetSync

---
+++ 956-number-of-music-playlists/number-of-music-playlists.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numMusicPlaylists(int n, int goal, int k) {
+        const int MOD = 1e9 + 7;
+
+        vector<vector<long>> dp(goal + 1, vector<long>(n + 1, 0));
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= goal; i++) {
+            for (int j = 1; j <= min(i, n); j++) {
+                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % MOD;
+                if (j > k) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % MOD;
+                }
+            }
+        }
+
+        return dp[goal][n];
+    }
+};
