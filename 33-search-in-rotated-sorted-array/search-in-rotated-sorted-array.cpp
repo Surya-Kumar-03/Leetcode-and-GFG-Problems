4254644b396cb3845a6477a808f0a916e2f0d25c
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-08 19:32:04 +0530

    Time: 3 ms (76.59%) | Memory: 11.1 MB (13.70%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //first step find the breaking point
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) / 2);
+            if(nums[mid] == target) return mid;
+            // we will perform binary search on whichever part is sorted
+            // it is guaranteed that one side is sorted
+            if(nums[low] <= nums[mid]) { 
+                //exists in this half
+                if(target >= nums[low] && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1; //pass to other half
+                }
+            } else {
+                //exists in this half
+                if(target <= nums[high] && target > nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1; //pass to other half
+                }
+            }
+        }
+        return -1;
+    }
+};
