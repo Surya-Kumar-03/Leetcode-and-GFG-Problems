671050d866b7422427683d03b068f63479b9a793
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-10 18:38:01 +0530

    Time: 5 ms (63.37%) | Memory: 14 MB (18.44%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+//     The idea is the same as the previous one without duplicates.
+// The only difference is that due to the existence of duplicates,
+// arr[l] == arr[mid] could be possible, the first half could be out of order
+// (i.e. not in the ascending order, e.g. {3, 1, 2, 3, 3, 3, 3})
+// we have to deal this case separately.
+// In that case, it is guaranteed that arr[high] also equal to arr[mid],
+// so the condition arr[mid] == arr[l] == arr[r] can be checked before the original logic,
+// if so then move left and right both towards the middle by 1 and repeat.
+
     bool search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
