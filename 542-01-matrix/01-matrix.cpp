76ce948813b2a5a8390fe97d87394290efac96a4
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-17 13:34:14 +0530

    Time: 54 ms (97.61%) | Memory: 27.5 MB (95.25%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,14 +4,16 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         
-        // other way in GFG of DP approach
         // https://practice.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+        // DP only works with nearest 0
+        // for nearest 1 only BFS will work (SEE GFG MANDATORY FOR BFS APPROACH)
         // can also be solved with BFS approach
         vector<vector<int>> dis(mat.begin(), mat.end());
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(dis[i][j] == 0) continue;
                 else {
+                    // you can use m + n here
                     int mini = INT_MAX;
                     if(i > 0) {
                         mini = min(mini, dis[i - 1][j]);
