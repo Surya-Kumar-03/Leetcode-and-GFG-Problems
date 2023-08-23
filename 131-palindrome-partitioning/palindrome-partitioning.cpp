094d3ff97d0eeedf6fe417dec485a94e38259bf3
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-23 19:54:16 +0530

    Time: 158 ms (27.98%) | Memory: 136.6 MB (17.58%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    bool isPalindrome(const string& s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end])
-                return false;
-            start++;
-            end--;
+    bool isPalindrome(string s) {
+        int si = 0;
+        int ei = s.size() - 1;
+        while(si < ei) {
+            if(s[si] != s[ei]) return false;
+            si++, ei--;
         }
         return true;
     }
 
-    void part(string& s, vector<vector<string>>& ans, int start, vector<string>& cur) {
+    void part(string& s, vector<vector<string>>& ans, int start, vector<string> cur)
+    {
         int n = s.size();
-        if (start == n) {
+        if(start == n) {
             ans.push_back(cur);
             return;
         }
 
-        for (int end = start; end < n; ++end) {
-            if (isPalindrome(s, start, end)) {
-                cur.push_back(s.substr(start, end - start + 1));
+        for(int end = start; end < n; end++) {
+            string curPart = s.substr(start, end - start + 1);
+            if(isPalindrome(curPart)){
+                cur.push_back(curPart);
                 part(s, ans, end + 1, cur);
                 cur.pop_back();
             }
@@ -28,8 +30,7 @@ public:
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> cur;
-        part(s, ans, 0, cur);
+        part(s, ans, 0, {});
         return ans;
     }
 };
