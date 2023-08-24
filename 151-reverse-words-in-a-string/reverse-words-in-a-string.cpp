546d73dd90a9fec8bad7d38a337ad2d73d8abbc2
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-24 14:25:47 +0530

    Time: 7 ms (48.38%) | Memory: 20.8 MB (28.39%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -9,20 +9,21 @@ public:
             if(ch != ' ') {
                 temp += ch;
             } else {
-                if(ans == "") {
+                if(ans == "") { //first word
                     ans = temp;
-                } else if(temp != "") {
+                } else if(temp != "") { // other than first word
                     ans = temp + " " + ans;
                 }
                 temp = "";
             }
         }
 
+        // Last word processing
         if(temp != "") {
-            if(ans == "") {
+            if(ans == "") { //has only one word
                 ans = temp;
             } else {
-                ans = temp + " " + ans;
+                ans = temp + " " + ans; //attach to front
             }
         } 
         return ans;
