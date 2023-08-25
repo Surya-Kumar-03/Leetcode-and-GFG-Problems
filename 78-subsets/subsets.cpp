c77fba60710d22bbd966af07b109aaf2f5fcc4d3
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-25 17:17:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (51.80%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     void generate(vector<int>& nums, int start, vector<int> cur, vector<vector<int>>& ans) {
         int n = nums.size();
-        if(start == n) {
-            ans.push_back(cur);
-            return;
+        ans.push_back(cur);
+        
+        for(int i = start; i < n; i++) {
+            cur.push_back(nums[i]);
+            generate(nums, i + 1, cur, ans);
+            cur.pop_back();
         }
-
-        generate(nums, start + 1, cur, ans);
-        cur.push_back(nums[start]);
-        generate(nums, start + 1, cur, ans);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
