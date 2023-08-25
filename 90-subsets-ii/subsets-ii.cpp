079eba70892440e6808e31487929a1537ee1fa65
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-25 17:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (47.20%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void subsets(vector<int>& nums, int start, vector<int> cur, vector<vector<int>>& ans) {
+        int n = nums.size();
+        ans.push_back(cur); 
+        for(int i = start; i < n; i++) {
+            cur.push_back(nums[i]); 
+            subsets(nums, i + 1, cur, ans);
+            cur.pop_back();  
+            while (i + 1 < n && nums[i] == nums[i + 1]) { //removes duplicates
+                i++;
+            }
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        subsets(nums, 0, {}, ans);
+        return ans;
+    }
+};
