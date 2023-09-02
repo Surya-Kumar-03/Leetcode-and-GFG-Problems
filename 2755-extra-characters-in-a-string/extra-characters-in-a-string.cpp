adf1b30f0739dda8fde1ddf4aa57115dda52095b
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-02 22:51:18 +0530

    Time: 79 ms (94.42%) | Memory: 62.3 MB (91.23%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index)
+    int memo(string& s, unordered_set<string>& mp, vector<int>& dp, int index)
     {
         if (index >= s.size()) return 0;
-        if (dp[index] != -1) return dp[index]; //use the stored results
+        if (dp[index] != -1) return dp[index]; 
         
         string currStr = "";
-        int minExtra = s.size();
+        int minExtra = s.size(); // max possible value
         for (int cutIdx = index; cutIdx < s.size(); cutIdx++)
         {
             currStr.push_back(s[cutIdx]);
-            //currStr will be a string from (index to cutIdx)
-            
-            //if string not in dictionary, we have to delete as they are extra chars
             int currExtra = (mp.count(currStr))? 0 : currStr.size();
-            int nextExtra = solve(s, mp, dp, cutIdx + 1);
+            int nextExtra = memo(s, mp, dp, cutIdx + 1);
             int totalExtra = currExtra + nextExtra;
             
             minExtra = min(minExtra, totalExtra);
         }
         return dp[index] = minExtra;
     }
+    
     int minExtraChar(string s, vector<string>& dictionary) 
     {
         vector<int>dp(s.size(), -1);
-        unordered_map<string, int>mp;
-        for (string& word : dictionary) mp[word]++;
-        
-        int ans = solve(s, mp, dp, 0);
+        unordered_set<string> mp(dictionary.begin(), dictionary.end());
+        int ans = memo(s, mp, dp, 0);
         return ans;
         
     }
