79b7bdb6832677608489db1861f501f65775d0ff
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-06 16:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (83.91%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> num;
-        for(int i = 1; i <= n; i++) {
-            num.push_back(i);
+        vector<int> options;
+        int nminusOneFact = 1;
+        for(int i = 1; i < n; i++) { //take nminusOneFact 
+            nminusOneFact *= i;
+            options.push_back(i);
         }
-
-        for(int i = 0; i < k - 1; i++) {
-            next_permutation(num.begin(), num.end());
-        }
+        options.push_back(n); //last number add to options
+        k -= 1; //to match our vector indexing
 
         string ans = "";
-        for(auto ele : num) {
-            ans += to_string(ele);
+        while(true) {
+            ans += to_string(options[(k / nminusOneFact)]);
+            options.erase(options.begin() + (k / nminusOneFact));
+            if(options.size() == 0) {
+                break;
+            }
+            
+            k %= nminusOneFact;
+            nminusOneFact = nminusOneFact / (options.size());
         }
         return ans;
+
+        // using stl
+        // vector<int> num;
+        // for(int i = 1; i <= n; i++) {
+        //     num.push_back(i);
+        // }
+
+        // for(int i = 0; i < k - 1; i++) {
+        //     next_permutation(num.begin(), num.end());
+        // }
+
+        // string ans = "";
+        // for(auto ele : num) {
+        //     ans += to_string(ele);
+        // }
+        // return ans;
     }
 };
