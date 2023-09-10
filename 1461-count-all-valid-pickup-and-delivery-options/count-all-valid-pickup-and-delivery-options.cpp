e8c18f8c2709e7401c95edf05353a01acf8d6f98
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-10 10:44:26 +0530

    Time: 2 ms (56.67%) | Memory: 6 MB (71.30%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int countOrders(int n) {
         long long res = 1, cap = 1000000007;
-        for (int i=1; i<n+1; ++i) res = res * i % cap;
-        for (int i=1; i<2*n; i+=2) res = res * i % cap;
+        for (int i=1; i<= n; i++) res = res * i % cap;
+        for (int i=1; i<=((2*n) - 1); i+=2) res = res * i % cap;
         return res;
     }   
 };
