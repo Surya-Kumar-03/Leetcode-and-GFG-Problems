d75cba3a5e0dbe2cc478a5c51d69eb001c0d0380
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-14 22:14:45 +0530

    Time: 35 ms (17.69%) | Memory: 24.3 MB (7.18%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool dfs(unordered_map<string, multiset<string>>& adj, string source, vector<string>& ans, int req) {
+        if (ans.size() == req) {
+            return true;
+        }
+
+        if (adj.find(source) != adj.end() && !adj[source].empty()) {
+            multiset<string> destinations = adj[source];
+            for (const string& dest : destinations) {
+                ans.push_back(dest);
+                adj[source].erase(adj[source].find(dest));
+
+                if (dfs(adj, dest, ans, req)) {
+                    return true;
+                }
+
+                ans.pop_back();
+                adj[source].insert(dest);
+            }
+        }
+
+        return false;
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        int req = tickets.size() + 1;
+        unordered_map<string, multiset<string>> adj;
+        
+        for (const vector<string>& ticket : tickets) {
+            string source = ticket[0];
+            string dest = ticket[1];
+            adj[source].insert(dest);
+        }
+
+        vector<string> ans;
+        ans.push_back("JFK");
+
+        if (dfs(adj, "JFK", ans, req)) {
+            return ans;
+        }
+
+        return vector<string>();
+    }
+};
