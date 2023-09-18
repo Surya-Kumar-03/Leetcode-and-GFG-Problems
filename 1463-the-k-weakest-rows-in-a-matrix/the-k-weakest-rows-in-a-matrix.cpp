267645f10adfe4cf984b8225a0fc2d14017e5d37
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-18 12:33:15 +0530

    Time: 9 ms (65.48%) | Memory: 11.2 MB (9.15%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    int findNumOnes(vector<int> input, int n) {
+        int low = 0, high = n - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) / 2);
+            if(input[mid] == 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         int m = mat.size();
         int n = mat[0].size();
 
         for(int i = 0; i < m; i++) {
-            int count = 0;
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 0) break;
-                count++;
-            }
+            int count = findNumOnes(mat[i], n);
             pq.push({count, i});
         }
 
