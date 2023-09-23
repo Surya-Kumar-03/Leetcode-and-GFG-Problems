37518c021b9996f8a295f533acf4c0de2da24dc2
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-23 23:59:35 +0530

    Time: 64 ms (94.02%) | Memory: 20.3 MB (44.19%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int findLongest(unordered_map<string, int>& dp, const unordered_set<string>& wordsSet, const string& word) {
-        if (dp.find(word) != dp.end()) {
-            return dp[word];
+    int findLongest(unordered_map<string, int>& memo, const unordered_set<string>& wordsSet, const string& word) {
+        if (memo.find(word) != memo.end()) {
+            return memo[word];
         }
 
         int maxLength = 1;
         for (int i = 0; i < word.size(); i++) {
             string curSubstr = word.substr(0, i) + word.substr(i + 1);
             if (wordsSet.find(curSubstr) != wordsSet.end()) {
-                maxLength = max(maxLength, 1 + findLongest(dp, wordsSet, curSubstr));
+                maxLength = max(maxLength, 1 + findLongest(memo, wordsSet, curSubstr));
             }
         }
 
-        dp[word] = maxLength;
+        memo[word] = maxLength;
         return maxLength;
     }
 
@@ -22,11 +22,11 @@ public:
         if (n == 1) return 1;
 
         unordered_set<string> wordsSet(words.begin(), words.end());
-        unordered_map<string, int> dp;
+        unordered_map<string, int> memo;
         int maxLength = 1;
 
         for (const string& word : words) {
-            maxLength = max(maxLength, findLongest(dp, wordsSet, word));
+            maxLength = max(maxLength, findLongest(memo, wordsSet, word));
         }
 
         return maxLength;
