b0d20de387514c4ebeb9fa1d915a1ed7f0a32680
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-25 10:18:58 +0530

    Time: 4 ms (48.11%) | Memory: 7.3 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int> chars;
-        for(char c : s) chars[c]++;
+        // unordered_map<char,int> chars;
+        // for(char c : s) chars[c]++;
 
-        for(char c : t) {
-            if(chars.find(c) == chars.end()) {
-                return c;
-            }
-            chars[c]--;
-            if(chars[c] == 0) chars.erase(c);
-        } 
-        
-        return '\0';
+        // for(char c : t) {
+        //     if(chars.find(c) == chars.end()) {
+        //         return c;
+        //     }
+        //     chars[c]--;
+        //     if(chars[c] == 0) chars.erase(c);
+        // } 
+
+        int sAscii = 0;
+        int tAscii = 0;
+
+        for(char c : s) sAscii += c;
+        for(char c : t) tAscii += c;
+
+        return (char) tAscii - sAscii;
     }
 };
