6976349ea90bde0b187ac6d327bfff05851bdc04
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-29 11:02:57 +0530

    Time: 135 ms (18.04%) | Memory: 96.6 MB (29.47%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
+        bool isInc = false;
+        bool isDec = false;
         int n = nums.size();
-        if(n <= 1) return true;
-        bool isAsc = true;
-        if(nums[0] > nums[1]) isAsc = false;
-        else if(nums[0] == nums[1]) {
-            int i = 0, j = 1;
-            while(j < n && nums[j] == nums[i]) {
-                j++, i++;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i - 1]) {
+                isInc = true;
             }
-            if(j == n) return true;
-            if(nums[i] > nums[j]) isAsc = false;
+            if(nums[i] < nums[i - 1]) {
+                isDec = true;
+            }
         }
-
-        for(int i = 2; i < n; i++) {
-            if(nums[i] < nums[i - 1] && isAsc) return false;
-            if(nums[i] > nums[i - 1] && !isAsc) return false;
-        }
-        return true;
+        return isInc && isDec ? false : true;
     }
+
+    // Brute Force
+    // bool isMonotonic(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n <= 1) return true;
+    //     bool isAsc = true;
+    //     if(nums[0] > nums[1]) isAsc = false;
+    //     else if(nums[0] == nums[1]) {
+    //         int i = 0, j = 1;
+    //         while(j < n && nums[j] == nums[i]) {
+    //             j++, i++;
+    //         }
+    //         if(j == n) return true;
+    //         if(nums[i] > nums[j]) isAsc = false;
+    //     }
+
+    //     for(int i = 2; i < n; i++) {
+    //         if(nums[i] < nums[i - 1] && isAsc) return false;
+    //         if(nums[i] > nums[i - 1] && !isAsc) return false;
+    //     }
+    //     return true;
+    // }
 };
