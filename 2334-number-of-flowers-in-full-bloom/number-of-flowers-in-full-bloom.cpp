eb9eb034585bb3a1fe50f4a929531e5902a26609
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-13 00:21:04 +0530

    Time: 287 ms (30.52%) | Memory: 89.4 MB (41.73%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -15,9 +15,8 @@ public:
 
         for (auto x : people)
         {
-            int t = x;
-            int idx1 = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int idx2 = lower_bound(end.begin(), end.end(), t) - end.begin();
+            int idx1 = upper_bound(start.begin(), start.end(), x) - start.begin();
+            int idx2 = lower_bound(end.begin(), end.end(), x) - end.begin();
             ans.push_back(idx1 - idx2);
         }
         return ans;
