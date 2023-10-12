fad6b26ac4ef4c1f2bf47e33fc3bfb52661d37dc
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-12 18:31:35 +0530

    Time: 242 ms (68.75%) | Memory: 89.5 MB (41.73%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -1,24 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> starts;
-        vector<int> ends;
-        
-        for (vector<int>& flower : flowers) {
-            starts.push_back(flower[0]);
-            ends.push_back(flower[1] + 1);
+    vector<int> fullBloomFlowers(vector<vector<int>> &flowers, vector<int> &people)
+    {
+        vector<int> start, end, ans;
+        for (auto x : flowers)
+        {
+            start.push_back(x[0]);
+            end.push_back(x[1]);
         }
-        
-        sort(starts.begin(), starts.end());
-        sort(ends.begin(), ends.end());
-        vector<int> ans;
-        
-        for (int person : people) {
-            int i = upper_bound(starts.begin(), starts.end(), person) - starts.begin();
-            int j = upper_bound(ends.begin(), ends.end(), person) - ends.begin();
-            ans.push_back(i - j);
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        for (auto x : people)
+        {
+            int t = x;
+            int idx1 = upper_bound(start.begin(), start.end(), t) - start.begin();
+            int idx2 = lower_bound(end.begin(), end.end(), t) - end.begin();
+            ans.push_back(idx1 - idx2);
         }
-        
         return ans;
     }
 };
