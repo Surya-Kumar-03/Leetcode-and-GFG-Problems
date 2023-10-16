e39e6e35e21cd61d76d8a1c1b8d8f385908b68a1
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-16 11:00:54 +0530

    Time: 2 ms (41.13%) | Memory: 6.7 MB (51.35%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     vector<int> getRow(int r) {
-        vector<vector<int>> ps;
-        ps.push_back({1});
-        for(int i = 1; i <= r; i++) {
-            vector<int> tmp;
-            tmp.push_back(1);
-            for(int j = 0; j < i - 1; j++) {
-                tmp.push_back(ps[i - 1][j] + ps[i - 1][j + 1]);
+        vector<int> a(r + 1);
+        a[0] = 1;
+        for(int i = 1; i < r + 1; i++) {
+            for(int j = i; j >= 1; j--) {
+                a[j] += a[j - 1];
             }
-            tmp.push_back(1);
-            ps.push_back(tmp);
         }
-        return ps[r];
+        return a;
     }
+
+    // vector<int> getRow(int r) {
+    //     vector<vector<int>> ps;
+    //     ps.push_back({1});
+    //     for(int i = 1; i <= r; i++) {
+    //         vector<int> tmp;
+    //         tmp.push_back(1);
+    //         for(int j = 0; j < i - 1; j++) {
+    //             tmp.push_back(ps[i - 1][j] + ps[i - 1][j + 1]);
+    //         }
+    //         tmp.push_back(1);
+    //         ps.push_back(tmp);
+    //     }
+    //     return ps[r];
+    // }
 };
