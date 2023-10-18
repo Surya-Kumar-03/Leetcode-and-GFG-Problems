3407ee2c9490f7d64b250d278f9c99833fc3d9e5
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-18 12:05:03 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,36 +7,65 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	int dfs(int source, vector<int> adj[], vector<bool>& visited, stack<int>& topo) {
-	    if(!visited[source]) {
-	        visited[source] = true;
+// 	int dfs(int source, vector<int> adj[], vector<bool>& visited, stack<int>& topo) {
+// 	    if(!visited[source]) {
+// 	        visited[source] = true;
 	        
-	        for(auto neigh : adj[source]) {
-	            dfs(neigh, adj, visited, topo);
-	        }
-	        topo.push(source);
-	    }
-	}
+// 	        for(auto neigh : adj[source]) {
+// 	            dfs(neigh, adj, visited, topo);
+// 	        }
+// 	        topo.push(source);
+// 	    }
+// 	}
 	
 	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    stack<int> topo;
-	    vector<bool> visited(V, false);
-	    for(int i = 0; i < V; i++) {
-	       if(!visited[i]) {
-	           dfs(i, adj, visited, topo);
-	       }
-	    }
+	   // stack<int> topo;
+	   // vector<bool> visited(V, false);
+	   // for(int i = 0; i < V; i++) {
+	   //    if(!visited[i]) {
+	   //        dfs(i, adj, visited, topo);
+	   //    }
+	   // }
 	    
-	    vector<int> ans;
-	    while(!topo.empty()) {
-	        int top = topo.top();
-	        ans.push_back(top);
-	        topo.pop();
-	    }
-	    return ans;
+	   // vector<int> ans;
+	   // while(!topo.empty()) {
+	   //     int top = topo.top();
+	   //     ans.push_back(top);
+	   //     topo.pop();
+	   // }
+	   // return ans;
+	   
+	   vector<int> indeg(V, 0);
+	   for(int i = 0; i < V; i++) {
+	       for(auto neigh : adj[i]) {
+	            indeg[neigh]++;
+	       }
+	   }
+	   
+	   queue<int> pending;
+	   vector<int> ans;
+	   for(int i = 0; i < V; i++) {
+	       if(indeg[i] == 0) {
+	           pending.push(i);
+	       }
+	   }
+	   
+	   while(!pending.empty()) {
+	       int top = pending.front();
+	       ans.push_back(top);
+	       pending.pop();
+	       
+	       for(auto neigh : adj[top]) {
+	           indeg[neigh]--;
+	           if(indeg[neigh] == 0) {
+	               pending.push(neigh);
+	           }
+	       }
+	   }
+	   return ans;
 	}
 };
 
