49dec5f5537f7141e525948403527899b040a605
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-19 12:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (39.46%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int m = s.size(), n = t.size();
-        string ss = "", tt = "";
-        for(int i = 0; i < m; i++) {
-            if(s[i] == '#') {
-                if(!ss.empty()) ss.pop_back();
-            } 
-            else ss += s[i];
-        }
-
-        for(int j = 0; j < n; j++) {
-            if(t[j] == '#') {
-                if(!tt.empty()) tt.pop_back();
-            }
-            else tt += t[j];
-        }
-
-        cout << ss << " " << tt << endl;
-
-        return (ss == tt);
-
-        // int i = s.size() - 1, j = t.size() - 1;
-        // while(true) {
+        // int m = s.size(), n = t.size();
+        // string ss = "", tt = "";
+        // for(int i = 0; i < m; i++) {
         //     if(s[i] == '#') {
-        //         int cur = 0;
-        //         do {
-        //             if(s[i] == '#') cur++;
-        //             else cur--; 
-        //             i--;
-        //         } while(i >= 0 && cur > 0);
-        //     } else if(t[j] == '#') {
-        //         int cur = 0;
-        //         do {
-        //             if(t[j] == '#') cur++;
-        //             else cur--; 
-        //             j--;
-        //         } while(j >= 0 && cur > 0);
-        //     } else {
-        //         if(s[i] != t[j]) return false;
-        //         i--, j--;
-        //     }
+        //         if(!ss.empty()) ss.pop_back();
+        //     } 
+        //     else ss += s[i];
         // }
-        // return true;
+
+        // for(int j = 0; j < n; j++) {
+        //     if(t[j] == '#') {
+        //         if(!tt.empty()) tt.pop_back();
+        //     }
+        //     else tt += t[j];
+        // }
+        // return (ss == tt);
+
+        int i = s.length() - 1, j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { 
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else break;
+            }
+            while (j >= 0) { 
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else break;
+            }
+
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--;
+            j--;
+        }
+        return true;
     }
 };
