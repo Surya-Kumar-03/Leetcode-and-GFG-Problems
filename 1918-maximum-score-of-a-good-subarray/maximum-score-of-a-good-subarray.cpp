b7bc6fd8f9aee50b8facbd231015b05a5d672afe
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-22 22:35:29 +0530

    Time: 143 ms (58.11%) | Memory: 89.8 MB (64.25%) - LeetSync

---
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = k;
+        int right = k;
+        int ans = nums[k];
+        int currMin = nums[k];
+        
+        while (left > 0 || right < n - 1) {
+            int l = left > 0 ? nums[left - 1]: 0;
+            int r = right < n - 1 ? nums[right + 1] : 0;
+            if (l < r) {
+                right++;
+                currMin = min(currMin, nums[right]);
+            } else {
+                left--;
+                currMin = min(currMin, nums[left]);
+            }
+            
+            ans = max(ans, currMin * (right - left + 1));
+        }
+        
+        return ans;
+    }
+};
