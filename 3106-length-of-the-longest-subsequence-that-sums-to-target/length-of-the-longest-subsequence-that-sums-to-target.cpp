d9db8d4e80b45b00c206e8e74f52e537e2411210
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-28 22:36:21 +0530

    Time: 512 ms (33.33%) | Memory: 151.9 MB (33.33%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-int solve(vector<int>& nums, int i, int target){
-    if(target == 0) return 0;                          // when target is zero that means we found set of elements that sum up to target   
-    if(target < 0 || i >= nums.size()) return -1001;   // if target is less then 0 or i is greater than nums size then its invalid case and just return with very small number     
-    if(dp[i][target] != - 1) return dp[i][target];     // if already seen this condition then just return the stored result
-    int take = 1 + solve(nums, i+1, target - nums[i]); // when we take, we add 1 to the count and decrease target by nums[i]
-    int notTake = solve(nums, i+1, target);            // when not-take, simple move to next element
-    return dp[i][target] = max(take, notTake);         // save the max result for future use
-}
-
-int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-    dp.resize(1001, vector<int>(1001, -1));            // initialise dp with -1
-    int ans = solve(nums, 0, target);                  // get the result with starting i = 0
-    return (ans > 0)?ans: -1;                          // return -1 if the ans is less than equal to 0, otherwise return ans
-}
-
-    // int lsubseq(vector<int>& nums, int i, int target, vector<vector<int>>& memo) {
-    //     if (target == 0) {
-    //         return 0;  
-    //     }
-    //     if (target < 0 || i < 0) {
-    //         return -1;
-    //     }
+    int lsubseq(vector<int>& nums, int i, int target, vector<vector<int>>& memo) {
+        if (target == 0) {
+            return 0;  
+        }
+        if (target < 0 || i >= nums.size()) {
+            return -10000;
+        }
         
-    //     if (memo[i][target] != -1) {
-    //         return memo[i][target]; 
-    //     }
+        if (memo[i][target] != -1) {
+            return memo[i][target]; 
+        }
         
-    //     int dontTake = lsubseq(nums, i - 1, target, memo);
-    //     if (nums[i] <= target) {
-    //         int take = lsubseq(nums, i - 1, target - nums[i], memo);
-    //         if (take != -1) {
-    //             take++; 
-    //         }
-    //         memo[i][target] = max(dontTake, take);
-    //     } else {
-    //         memo[i][target] = dontTake;
-    //     }
+        int take = 1 + lsubseq(nums, i + 1, target - nums[i], memo);
+        int dontTake = lsubseq(nums, i + 1, target, memo);
         
-    //     return memo[i][target];
-    // }
+        return memo[i][target] = max(take, dontTake);
+    }
     
-    // int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-    //     int n = nums.size();
-    //     vector<vector<int>> memo(n, vector<int>(target + 1, -1));
-    //     return lsubseq(nums, n - 1, target, memo);
-    // }
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> memo(n, vector<int>(target + 1, -1));
+        int ans = lsubseq(nums, 0, target, memo);
+        if(ans < 0) return -1;
+        else return ans;
+    }
 };
