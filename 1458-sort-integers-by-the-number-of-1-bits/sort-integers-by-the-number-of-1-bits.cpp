160ded72d81ff43501758a0f9acae63cce38e158
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-30 11:13:09 +0530

    Time: 21 ms (7.42%) | Memory: 10.4 MB (66.29%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
+    static int getSetBits(int num) {
+        int setBits = 0;
+        for(int i = 0; i <= 31; i++) {
+            if((num >> i) & 1) setBits++;
+        }
+        return setBits;
+    }
+
     static bool comp(int& a, int& b) {
-        int setA = __builtin_popcount(a);
-        int setB = __builtin_popcount(b);
+        // int setA = __builtin_popcount(a);
+        // int setB = __builtin_popcount(b);
+
+        int setA = getSetBits(a);
+        int setB = getSetBits(b);
         if(setA == setB) {
             return a < b;
         } else return setA < setB;
