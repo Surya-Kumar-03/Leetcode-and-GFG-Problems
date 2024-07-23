7c1e4721d84ac954b57b975700688734960c921e
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2024-07-23 21:01:35 +0530

    Time: 8 ms (34.06%) | Memory: 8.7 MB (53.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,22 +3,33 @@ public:
     bool isPalindrome(string s) {
         int n = s.size();
         int i = 0, j = n - 1;
-        while(i < j) {
-            while (i < j && !((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))) {
+        while (i < j) {
+            while (i < j && !((s[i] >= 'A' && s[i] <= 'Z') ||
+                              (s[i] >= 'a' && s[i] <= 'z') ||
+                              (s[i] >= '0' && s[i] <= '9'))) {
                 i++;
             }
 
-            while (i < j && !((s[j] >= 'A' && s[j] <= 'Z') || (s[j] >= 'a' && s[j] <= 'z') || (s[j] >= '0' && s[j] <= '9'))) {
+            while (i < j && !((s[j] >= 'A' && s[j] <= 'Z') ||
+                              (s[j] >= 'a' && s[j] <= 'z') ||
+                              (s[j] >= '0' && s[j] <= '9'))) {
                 j--;
             }
 
-            cout << s[i] << " " << s[j] << endl;
-            if(tolower(s[i]) != tolower(s[j])) {
+            char char1 = s[i];
+            char char2 = s[j];
+
+            if (char1 >= 'A' && char1 <= 'Z')
+                char1 += 'a' - 'A'; 
+            if (char2 >= 'A' && char2 <= 'Z')
+                char2 += 'a' - 'A'; 
+
+            if (char1 != char2) {
                 return false;
             }
 
             i++, j--;
         }
         return true;
-    } 
+    }
 };
