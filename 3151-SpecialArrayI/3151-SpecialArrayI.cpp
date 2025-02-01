780dcd010004d9568e8ac26e1413ab957eb174c2
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2025-02-02 01:58:08 +0530

    Create file

---
+++ 3151-SpecialArrayI/3151-SpecialArrayI.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return true;
+        }
+
+        for(int i = 1; i < n; i++) {
+            if (nums[i] % 2 == 0 && nums[i - 1] % 2 == 0) {
+                return false;
+            } else if (nums[i] % 2 == 1 && nums[i - 1] % 2 == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
