3ba23750a9c39dbd680afbf323034be2f7d4cb83
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2025-02-21 01:05:11 +0530

    File updated at 2/21/2025, 1:04:56 AM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool binaryBacktracker(unordered_set<string>& numsSet, string& ans) {
-        if(ans.size() == numsSet.size()) {
-            if (numsSet.find(ans) == numsSet.end()) {
-                return true;
-            } else {
-                return false;
-            }
-        } 
+    // bool binaryBacktracker(unordered_set<string>& numsSet, string& ans) {
+    //     if(ans.size() == numsSet.size()) {
+    //         if (numsSet.find(ans) == numsSet.end()) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     } 
 
 
-        for(char ch : {'0', '1'}) {
-            ans += ch;
-            if (binaryBacktracker(numsSet, ans)) {
-                return true;
-            }
-            ans.pop_back();
-        }
+    //     for(char ch : {'0', '1'}) {
+    //         ans += ch;
+    //         if (binaryBacktracker(numsSet, ans)) {
+    //             return true;
+    //         }
+    //         ans.pop_back();
+    //     }
 
-        return false;
-    }
+    //     return false;
+    // }
 
     string findDifferentBinaryString(vector<string>& nums) {
         // unordered_set<string> numsSet(nums.begin(), nums.end());
