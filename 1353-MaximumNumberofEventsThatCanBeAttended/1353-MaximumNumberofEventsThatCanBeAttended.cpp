f4f18dd19df7292ae1b52e491888a64c5261e4a7
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2025-07-08 00:07:32 +0530

    Create 1353-MaximumNumberofEventsThatCanBeAttended

---
+++ 1353-MaximumNumberofEventsThatCanBeAttended/1353-MaximumNumberofEventsThatCanBeAttended.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/8/2025, 12:07:23 AM
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        // Sort events based on start day
+        sort(events.begin(), events.end());
+        
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        int day = 0, index = 0, n = events.size(), result = 0;
+        
+        while (!minHeap.empty() || index < n) {
+            if (minHeap.empty()) {
+                day = events[index][0];
+            }
+            while (index < n && events[index][0] <= day) {
+                minHeap.push(events[index][1]);
+                index++;
+            }
+            minHeap.pop();
+            result++;
+            day++;
+            
+            while (!minHeap.empty() && minHeap.top() < day) {
+                minHeap.pop();
+            }
+        }
+        return result;
+    }
+};
