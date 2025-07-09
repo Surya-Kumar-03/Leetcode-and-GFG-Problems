75d61d58b1f9ebbb73abb55f47d7630c4a34ce74
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2025-07-09 23:14:16 +0530

    Create 3439-RescheduleMeetingsforMaximumFreeTimeI

---
+++ 3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2025, 11:14:07 PM
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        int count = startTime.size();
+        vector<int> prefixSum(count + 1, 0);
+        int maxFree = 0;
+
+        for (int i = 0; i < count; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + (endTime[i] - startTime[i]);
+        }
+
+        for (int i = k - 1; i < count; ++i) {
+            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
+            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
+            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
+            int freeTime = windowEnd - windowStart - occupied;
+            maxFree = max(maxFree, freeTime);
+        }
+
+        return maxFree;
+    }
+};
